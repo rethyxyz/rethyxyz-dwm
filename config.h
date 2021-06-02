7819d44f8d9e3b45d66a6c92abaffc17e947d3e0
Brody Rethy <brodyrethy@gmail.com>
2021-06-02 19:59:46 -0300

    Pushing changes.

--- config.h
+++ config.h
@@ -1,7 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int gappx     = 16;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
@@ -41,6 +41,7 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
+    { "mpv",      NULL,       NULL,       1 << 3,       1,           -1 },
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
 	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
 	{ "qutebrowser", NULL,    NULL,       1 << 8,       0,           -1 },
@@ -71,39 +72,52 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
+
+static const char *bright_down[]  = { "light", "-U", "5", NULL };
+static const char *bright_up[]  = { "light", "-A", "5", NULL };
+
 static const char *browsercmd[]  = { "qutebrowser", NULL };
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-
-static const char *bright_up[]  = { "light", "-A", "5", NULL };
-static const char *bright_down[]  = { "light", "-U", "5", NULL };
-
 static const char *helpmenu[]  = { "zathura", "~/Documents/Repositories/dwm/README.pdf", NULL };
+
+static const char *logout_request_handler[]  = { "logout_request_handler.sh", NULL };
+
 static const char *mnt_drive[]  = { "st", "-e", "mnt_drive.sh", NULL };
 
 static const char *mpc_next[]  = { "mpc", "-p", "6601", "next", NULL };
-static const char *mpc_toggle[]  = { "mpc", "-p", "6601", "toggle", NULL };
 static const char *mpc_prev[]  = { "mpc", "-p", "6601", "prev", NULL };
-static const char *mpc_seek_backward[]  = { "mpc", "-p", "6601", "seek", "-1%", NULL };
-static const char *mpc_seek_forward[]  = { "mpc", "-p", "6601", "seek", "+1%", NULL };
 
+static const char *mpc_seek_forward[]  = { "mpc", "-p", "6601", "seek", "+1%", NULL };
+static const char *mpc_seek_backward[]  = { "mpc", "-p", "6601", "seek", "-1%", NULL };
+static const char *mpc_toggle[]  = { "mpc", "-p", "6601", "toggle", NULL };
+
+static const char *vol_up[]  = { "pactl", "set-sink-volume", "0", "+1%", NULL };
+static const char *vol_down[]  = { "pactl", "set-sink-volume", "0", "-1%", NULL };
+static const char *vol_mute[]  = { "pactl", "set-sink-mute", "0", "toggle", NULL };
+
+static const char *picard[]  = { "picard", NULL };
+static const char *ranger[]  = { "st", "-e", "ranger", NULL };
 static const char *ncmpcpp[]  = { "st", "-e", "ncmpcpp", NULL };
 static const char *neomutt[]  = { "st", "-e", "neomutt", NULL };
 static const char *newsboat[]  = { "st", "-e", "newsboat", NULL };
-static const char *picard[]  = { "picard", NULL };
-static const char *random_wallpaper[]  = { "randomize_wallpaper.sh", NULL };
-static const char *ranger[]  = { "st", "-e", "ranger", NULL };
+static const char *restart_dwmbar[]  = { "restart_dwmbar.sh", NULL };
+static const char *restart_request_handler[]  = { "restart_request_handler.sh", NULL };
 static const char *scratchpad[]  = { "st", "-e", "vim", "~/Documents/Notes/Scratchpad", NULL };
+static const char *slock[]  = { "slock", NULL };
 static const char *soulseek[]  = { "soulseekqt", NULL };
 static const char *termcmd[]  = { "st", NULL };
-static const char *title_case[]  = { "st", "-e", "title_case.sh", NULL };
-static const char *todo[]  = { "st", "-e", "vim", "~/Documents/Notes/TODO", NULL };
+static const char *toggle_transparency[]  = { "toggle_transparency.sh", NULL };
 
-static const char *vol_up[]  = { "pactl", "set-sink-volume", "1", "+1%", NULL };
-static const char *vol_down[]  = { "pactl", "set-sink-volume", "1", "-1%", NULL };
-static const char *vol_mute[]  = { "pactl", "set-sink-mute", "1", "toggle", NULL };
+/*
+ * static const char *mnt_luks_drive[]  = { "st", "-e", "mnt_luks_drive.sh", NULL };
+ * static const char *title_case[]  = { "st", "-e", "title_case.sh", NULL };
+ * static const char *random_wallpaper[]  = { "randomize_wallpaper.sh", NULL };
+*/
 
 static Key keys[] = {
 	/* modifier                     key        function			argument */
+
+    /* Workspace */
 	TAGKEYS(                        XK_1,						0)
 	TAGKEYS(                        XK_2,						1)
 	TAGKEYS(                        XK_3,						2)
@@ -113,61 +127,65 @@ static Key keys[] = {
 	TAGKEYS(                        XK_7,						6)
 	TAGKEYS(                        XK_8,						7)
 	TAGKEYS(                        XK_9,						8)
+
+    /* F1-12 */
 	{ 0,							XK_F1,		togglefullscr,	{0} },
 	{ 0,							XK_F4,		zoom,			{0} },
 	{ 0,							XK_F6,		killclient,		{0} },
 	{ 0,							XK_F9,		spawn,			{.v = helpmenu } },
 	{ 0,							XK_F11,		focusstack,		{.i = -1 } },
 	{ 0,							XK_F12,		focusstack,		{.i = +1 } },
-	{ 0, XF86XK_AudioLowerVolume,	spawn,						{.v = vol_down } },
+
+    /* XF86 */
 	{ 0, XF86XK_AudioMute,			spawn,						{.v = vol_mute } },
-	{ 0, XF86XK_AudioNext,			spawn,						{.v = mpc_next } },
-	{ 0, XF86XK_AudioPlay,			spawn,						{.v = mpc_toggle } },
-	{ 0, XF86XK_AudioPrev,			spawn,						{.v = mpc_prev } },
+	{ 0, XF86XK_AudioLowerVolume,	spawn,						{.v = vol_down } },
 	{ 0, XF86XK_AudioRaiseVolume,	spawn,						{.v = vol_up } },
-	{ 0, XF86XK_AudioStop,			spawn,						{.v = ncmpcpp } },
 	{ 0, XF86XK_MonBrightnessDown,	spawn,						{.v = bright_down } },
 	{ 0, XF86XK_MonBrightnessUp,	spawn,						{.v = bright_up } },
-	{ MODKEY,						XK_0,		view,			{.ui = ~0 } },
+	{ 0, XF86XK_AudioStop,			spawn,						{.v = ncmpcpp } },
+	{ 0, XF86XK_AudioPrev,			spawn,						{.v = mpc_prev } },
+	{ 0, XF86XK_AudioPlay,			spawn,						{.v = mpc_toggle } },
+	{ 0, XF86XK_AudioNext,			spawn,						{.v = mpc_next } },
+
+    /* Mod (using Mod4Mask) */
 	{ MODKEY,						XK_F1,		spawn,			{.v = vol_mute } },
-	{ MODKEY,						XK_F10,		spawn,			{.v = mpc_prev } },
-	{ MODKEY,						XK_F11,		spawn,			{.v = mpc_toggle } },
-	{ MODKEY,						XK_F12,		spawn,			{.v = mpc_next } },
 	{ MODKEY,						XK_F2,		spawn,			{.v = vol_down } },
 	{ MODKEY,						XK_F3,		spawn,			{.v = vol_up } },
 	{ MODKEY,						XK_F5,		spawn,			{.v = bright_down } },
 	{ MODKEY,						XK_F6,		spawn,			{.v = bright_up } },
 	{ MODKEY,						XK_F9,		spawn,			{.v = ncmpcpp } },
-	{ MODKEY,						XK_Return,	spawn,			{.v = termcmd } },
-	{ MODKEY,						XK_Tab,		view,			{0} },
+	{ MODKEY,						XK_F10,		spawn,			{.v = mpc_prev } },
+	{ MODKEY,						XK_F11,		spawn,			{.v = mpc_toggle } },
+	{ MODKEY,						XK_F12,		spawn,			{.v = mpc_next } },
 	{ MODKEY,						XK_b,		spawn,			{.v = browsercmd } },
-	{ MODKEY,						XK_comma,	spawn,			{.v = mpc_seek_backward } },
 	{ MODKEY,						XK_d,		incnmaster,		{.i = -1 } },
-	{ MODKEY,						XK_equal,	setgaps,		{.i = +1 } },
 	{ MODKEY,						XK_f,		setlayout,		{.v = &layouts[1]} },
 	{ MODKEY,						XK_h,		focusmon,		{.i = -1 } },
 	{ MODKEY,						XK_i,		incnmaster,     {.i = +1 } },
 	{ MODKEY,						XK_j,		focusstack,		{.i = +1 } },
 	{ MODKEY,						XK_k,		focusstack,		{.i = -1 } },
 	{ MODKEY,						XK_l,		focusmon,		{.i = +1 } },
-	{ MODKEY,						XK_minus,	setgaps,        {.i = -1 } },
 	{ MODKEY,						XK_n,		spawn,			{.v = ncmpcpp } },
 	{ MODKEY,						XK_p,		spawn,          {.v = dmenucmd } },
-	{ MODKEY,						XK_period,	spawn,			{.v = mpc_seek_forward } },
 	{ MODKEY,						XK_r,		spawn,			{.v = ranger } },
 	{ MODKEY,						XK_s,		spawn,			{.v = soulseek } },
-	{ MODKEY,						XK_space,   setlayout,      {0} },
 	{ MODKEY,						XK_t,	    setlayout,      {.v = &layouts[0]} },
-	{ MODKEY|ControlMask,			XK_h,		setmfact,		{.f = -0.01} },
-	{ MODKEY|ControlMask,			XK_l,		setmfact,		{.f = +0.01} },
-	{ MODKEY|ControlMask,			XK_m,		spawn,			{.v = mnt_drive } },
-	{ MODKEY|ControlMask,			XK_t,		spawn,			{.v = title_case } },
-	{ MODKEY|ShiftMask,				XK_0,		tag,			{.ui = ~0 } },
-	{ MODKEY|ShiftMask,				XK_Return,	zoom,           {0} },
+ 	{ MODKEY,                       XK_m,		setlayout,		{.v = &layouts[2]} },
+	{ MODKEY,						XK_0,		view,			{.ui = ~0 } },
+	{ MODKEY,						XK_Return,	spawn,			{.v = termcmd } },
+	{ MODKEY,						XK_Tab,		view,			{0} },
+	{ MODKEY,						XK_comma,	spawn,			{.v = mpc_seek_backward } },
+	{ MODKEY,						XK_equal,	setgaps,		{.i = +1 } },
+	{ MODKEY,						XK_minus,	setgaps,        {.i = -1 } },
+	{ MODKEY,						XK_period,	spawn,			{.v = mpc_seek_forward } },
+	{ MODKEY,						XK_space,   setlayout,      {0} },
+
+    /* Mod+Alt */
+	{ MODKEY|Mod1Mask,				XK_l,		spawn,			{.v = slock } },
+
+    /* Mod+Shift */
 	{ MODKEY|ShiftMask,				XK_b,		togglebar,		{0} },
-	{ MODKEY|ShiftMask,				XK_c,		killclient,		{0} },
-	{ MODKEY|ShiftMask,				XK_c,		quit,			{0} },
-	{ MODKEY|ShiftMask,				XK_equal,	setgaps,		{.i = 0  } },
+	{ MODKEY|ShiftMask,				XK_c,		spawn,			{.v = logout_request_handler } },
 	{ MODKEY|ShiftMask,				XK_f,		togglefullscr,	{0} },
 	{ MODKEY|ShiftMask,				XK_h,		tagmon,			{.i = -1 } },
 	{ MODKEY|ShiftMask,				XK_l,		tagmon,			{.i = +1 } },
@@ -175,13 +193,19 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,				XK_n,		spawn,			{.v = newsboat } },
 	{ MODKEY|ShiftMask,				XK_p,		spawn,          {.v = picard } },
 	{ MODKEY|ShiftMask,				XK_q,		killclient,		{0} },
-	{ MODKEY|ShiftMask,				XK_r,		spawn,			{.v = random_wallpaper } },
+	{ MODKEY|ShiftMask,				XK_r,		spawn,			{.v = restart_dwmbar } },
 	{ MODKEY|ShiftMask,				XK_s,		spawn,			{.v = scratchpad } },
-	{ MODKEY|ShiftMask,				XK_t,		spawn,			{.v = todo } },
- 	{ MODKEY,                       XK_0,		view,			{.ui = ~0 } },
- 	{ MODKEY,                       XK_m,		setlayout,		{.v = &layouts[2]} },
- 	{ MODKEY|ShiftMask,             XK_0,		tag,			{.ui = ~0 } },
+	{ MODKEY|ShiftMask,				XK_t,		spawn,			{.v = toggle_transparency } },
+	{ MODKEY|ShiftMask,				XK_Return,	zoom,           {0} },
+	{ MODKEY|ShiftMask,			    XK_BackSpace,	spawn,      {.v = restart_request_handler } },
+	{ MODKEY|ShiftMask,				XK_equal,	setgaps,		{.i = 0  } },
  	{ MODKEY|ShiftMask,             XK_space,	togglefloating,	{0} },
+	{ MODKEY|ShiftMask,				XK_0,		tag,			{.ui = ~0 } },
+
+    /* Mod+Ctrl base binds*/
+	{ MODKEY|ControlMask,			XK_h,		setmfact,		{.f = -0.01} },
+	{ MODKEY|ControlMask,			XK_l,		setmfact,		{.f = +0.01} },
+	{ MODKEY|ControlMask,			XK_m,		spawn,			{.v = mnt_drive } },
 };
 
 /* button definitions */
